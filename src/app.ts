8bec508e3d5e34be38a8d818b976d88c38efca84
Cmoros <cmoros@fiuba.ar>
2023-03-11 07:16:55 -0300

    Working docker and server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import http from "http";
-
 const hostname = "localhost";
+// const hostname = "127.0.0.1";
+
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -9,6 +10,10 @@ const server = http.createServer((req, res) => {
   res.end("Hello World\n");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(
+  port,
+  // hostname, // If I set the hostname, for some reason, docker won't work properly
+  () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  }
+);
